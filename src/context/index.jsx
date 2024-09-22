c240a1fbcbce12f9da5dc04ebe232e6511d2e019
Khvede93 <gio.khvede93@gmail.com>
2024-09-23 01:37:44 +0200

    add error and loading state, add some error checking

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,23 +4,31 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function handleSubmit(e) {
     e.preventDefault();
+    setLoading(true);
+    setError(null);
     try {
       const response = await fetch(
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
       );
+      if (!response.ok) {
+        throw new Error(`Something went wrong | ${response.status}`);
+      }
       const data = await response.json();
-      console.log(data);
     } catch (e) {
-      console.log(e);
+      setError(e.message);
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSubmit }}
+      value={{ searchParam, setSearchParam, handleSubmit, loading, error }}
     >
       {children}
     </GlobalContext.Provider>
