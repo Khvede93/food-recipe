b2c6860cecf6be2bfc6f2382cfd5f810977b2537
Khvede93 <gio.khvede93@gmail.com>
2024-09-22 22:36:06 +0200

    add temp default routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import { useState } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './components';
+import { Details, Favorites, Home } from './pages';
 
 function App() {
-  return <div className='bg-red-500'>App</div>;
+  return (
+    <div>
+      <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='/recipe-item/:id' element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { Home } from './home';
+export { Favorites } from './favorites';
+export { Details } from './details';
