c2d015ce15f7d5f57e9d9aad7676710f17a6e22e
Khvede93 <gio.khvede93@gmail.com>
2024-09-23 00:10:48 +0200

    add navbar temp stucture // input needs value and onchange

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='flex flex-col lg:flex-row justify-between items-center py-8 container mx-auto gap-5 lg:gap-0'>
+      <h2 className='text-2xl font-semibold'>Site Logo</h2>
+      <form>
+        <input
+          type='text'
+          name='search'
+          placeholder='Enter Items ...'
+          className='p-3 px-8 rounded-full outline-none lg:w-96 bg-white/75 shadow-lg shadow-gray-100 focus:shadow-gray-200'
+        />
+      </form>
+    </nav>
+  );
 };
