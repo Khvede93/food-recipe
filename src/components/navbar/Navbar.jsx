3751b68e067a0900c642991a73495727a6022a72
Khvede93 <gio.khvede93@gmail.com>
2024-09-23 00:23:47 +0200

    Temp navbar is done // need to handle input

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <nav className='flex flex-col lg:flex-row justify-between items-center py-8 container mx-auto gap-5 lg:gap-0'>
-      <h2 className='text-2xl font-semibold'>Site Logo</h2>
+      <h2 className='text-2xl font-semibold'>
+        <NavLink to={'/'}>Site Logo</NavLink>
+      </h2>
       <form>
         <input
           type='text'
@@ -12,6 +15,24 @@ export const Navbar = () => {
           className='p-3 px-8 rounded-full outline-none lg:w-96 bg-white/75 shadow-lg shadow-gray-100 focus:shadow-gray-200'
         />
       </form>
+      <ul className='flex gap-5'>
+        <li>
+          <NavLink
+            to={'/'}
+            className='text-black hover:text-gray-700 duration-300'
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={'/favorites'}
+            className='text-black hover:text-gray-700 duration-300'
+          >
+            Favorites
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
