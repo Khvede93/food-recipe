e06f4d9318279020ae1f3ecb868020a3359cedc4
Khvede93 <gio.khvede93@gmail.com>
2024-09-23 01:33:50 +0200

    reading recipes successfully

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,14 +3,15 @@ import { NavLink } from 'react-router-dom';
 import { GlobalContext } from '../../context';
 
 export const Navbar = () => {
-  const { searchParam, setSearchParam } = useContext(GlobalContext);
+  const { searchParam, setSearchParam, handleSubmit } =
+    useContext(GlobalContext);
 
   return (
     <nav className='flex flex-col lg:flex-row justify-between items-center py-8 container mx-auto gap-5 lg:gap-0'>
       <h2 className='text-2xl font-semibold'>
         <NavLink to={'/'}>Site Logo</NavLink>
       </h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           autoComplete='off'
           value={searchParam}
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -5,8 +5,23 @@ export const GlobalContext = createContext(null);
 export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState('');
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
-    <GlobalContext.Provider value={{ searchParam, setSearchParam }}>
+    <GlobalContext.Provider
+      value={{ searchParam, setSearchParam, handleSubmit }}
+    >
       {children}
     </GlobalContext.Provider>
   );
