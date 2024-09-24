71fe8f4d3bf44a16c0626bfdb739dd7a5e4416e1
Khvede93 <gio.khvede93@gmail.com>
2024-09-24 21:08:30 +0200

    favorites list temp version

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -59,6 +59,7 @@ export default function GlobalState({ children }) {
         recipeDetails,
         setRecipeDetails,
         handleAddToFavorites,
+        favoritesList,
       }}
     >
       {children}
--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../context';
+import { RecipeItem } from '../../components';
 
 export const Favorites = () => {
-  return <div>Favorites</div>;
+  const { favoritesList } = useContext(GlobalContext);
+
+  return (
+    <div className='py-8 container mx-auto flex flex-wrap justify-center gap-10'>
+      {favoritesList && favoritesList.length ? (
+        favoritesList.map((item) => <RecipeItem key={item.id} item={item} />)
+      ) : (
+        <div>
+          <p className='lg:text-4xl text-xl text-black font-extrabold'>
+            Nothing is Added in Favorites.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
